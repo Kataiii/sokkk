be0b6822005f457a004d3ae1eb793c8153e9be4c
Jho00 <akutin_artem@mail.ru>
2022-10-08 18:22:38 +0400

    add events

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -11,4 +11,46 @@ export class AppController {
 	getHello(): string {
 		return this.appService.getHello();
 	}
+
+	@Get('events')
+	getEvents() {
+		return {
+			upcomingEvents: [
+				{
+					name: "Квиз по случаю дня рождения департамента",
+					date: "22.10.22"
+				},
+				{
+					name: "Совместный поход на пейнтбол",
+					date: "25.10.22"
+				}
+			],
+			events: [
+				{
+					name: "Квиз по случаю дня рождения департамента",
+					date: "22.10.22"
+				},
+				{
+					name: "Совместный поход на пейнтбол",
+					date: "25.10.22"
+				},
+				{
+					name: "Баня на пароме",
+					date: "27.10.22"
+				},
+				{
+					name: "Квиз 18+",
+					date: "15.11.22"
+				},
+				{
+					name: "Прятки в темноте",
+					date: "19.11.22"
+				},
+				{
+					name: "Поход в Бар 100 рентген",
+					date: "24.11.22"
+				}
+			]
+		}
+	}
 }
