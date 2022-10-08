f2c4cb978192f520e9e864a6906a56261007f386
Artem <jho00@Artems-MacBook-Pro.local>
2022-10-08 17:10:27 +0400

    api wrapper

---
+++ server/src/api/api.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { ApiService } from './api.service';
+import { GenerateNftDTO } from './dto';
+import { NewWalletResposne } from './types';
+
+@Controller('api')
+export class ApiController {
+  constructor(private readonly apiService: ApiService) {}
+
+  @Post('new-wallet')
+  getHello(): Promise<NewWalletResposne> {
+    return this.apiService.makeNewWallet();
+  }
+
+  @Get('balance/:publicKey')
+  getWalletBalance(@Param() params) {
+      return this.apiService.getWalletBalance(params.publicKey)
+  }
+
+  @Get('balance/nft/:publicKey')
+  getWalletNFT(@Param() params) {
+      return this.apiService.getWalletNFT(params.publicKey)
+  }
+
+  @Get('transaction/:transactionHash/status')
+  getTransactionStatus(@Param() params) {
+      return this.apiService.transactionStatus({transactionHash: params.transactionHash})
+  }
+
+  @Get('nft/:tokendId/')
+  getNFTInfo(@Param() params) {
+      return this.apiService.getNFTInfo(params.tokendId);
+  }
+
+  @Post('nft/generate')
+  generateNewNFT(@Body() dto: GenerateNftDTO) {
+      return this.apiService.sendNFT(dto);
+  }
+}
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { getMongoConfig } from './configs/mongo.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ApiModule } from './api/api.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     MongooseModule.forRootAsync(getMongoConfig()),
     AuthModule,
     UserModule,
+    ApiModule,
   ],
   controllers: [AppController],
   providers: [AppService],
