79df5e67e393024bdd10a891c55ee0397df36db9
les_ne <nevezhina-olesya@mail.ru>
2022-10-08 10:12:53 +0400

    updateEntity

--- server/src/main/java/com/moretech/server/service/UserDetailsImpl.java
+++ server/src/main/java/com/moretech/server/service/UserDetailsImpl.java
@@ -5,6 +5,7 @@ import com.moretech.server.models.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 import java.util.List;
@@ -29,6 +30,7 @@ public class UserDetailsImpl implements UserDetails {
     }
 
     public static UserDetailsImpl build(User user){
+
         List<GrantedAuthority> authorities = user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName().name()))
                 .collect(Collectors.toList());
